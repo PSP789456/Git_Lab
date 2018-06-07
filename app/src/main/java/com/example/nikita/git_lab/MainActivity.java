b8288ba9e0594ab36b5e57a56378f2bd45e7b857
Mykyta Holik <psp789456@yahoo.com>
2018-06-07 20:04:34 +0200

    Initial commit

---
+++ app/src/main/java/com/example/nikita/git_lab/MainActivity.java
@@ -0,0 +1,21 @@
+package com.example.nikita.git_lab;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.CheckBox;
+import android.widget.Spinner;
+
+import com.example.nikita.Git_Lab.R;
+
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+}
